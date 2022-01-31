ae916ad4f8a47cd71ad725772c0818d67affbed1
Rose Hastie <roseojehastie@gmail.com>
2022-01-31 17:04:42 +0000

    Js-practice

---
+++ function-1.js
@@ -0,0 +1,65 @@
+
+//function are repeatable block of code
+
+// function multiply (a, b) {
+//     return 2 * 8
+// }
+// console.log(multiply)
+
+// const result = (5 * 10)
+// console.log(result)
+// let add = (5 + 8)
+// console.log(add)
+
+// document.getElementById("demo").innerHTML = "Hello JavaScript!"
+
+// let sessionParagraph = document.getElementById("session")
+
+// function practice(){
+//     // console.log("button clicked")
+//     sessionParagraph.textContent = "Practice make perfect"
+// }
+
+// let demoParagraph = document.getElementById("demo")
+
+// function demo() {
+//     demoParagraph.textContent = "Hello Dolly";
+// }
+
+// let greetParagraph = document.getElementById("greet")
+
+// function greet() {
+//     greetParagraph.textContent = "I ❤️ you"; // used text.Content
+// }
+// function greet() {
+//     greetParagraph.innerHTML = "I ❤🥰 you"; // out of curiosity tried innerHTML
+// }
+
+// let a, b, c;
+// a = 10; b = 2; c = a + b;
+
+//  document.getElementById("demo1").innerHTML = c;
+
+//  let y, x, z;
+//  y = 12,
+//   x = 9,
+//   z = y + z;
+//  document.getElementById("cal").innerHTML = z;
+
+//  let a, b, c;
+
+//  a = 12; b = 9; c = a + b;
+
+//  document.getElementById("addition").innerHTML = c;
+ 
+ //js white space
+//  let person = "Hegde"
+//  let person="Hegde"
+//  console.log(my name: is + person)
+// document.getElementById("person").innerHTML = person;
+
+// let x = 5 + 2 + 3;
+// document.getElementById("jsvariable").innerHTML=x; 
+
+// let x =  2 + 3 + "5";
+// document.getElementById("note").innerHTML = x;
