eda40804692c89e874fed9de11c6e8c556971c58
Rose Hastie <roseojehastie@gmail.com>
2022-02-02 16:32:01 +0000

    add

---
+++ console.js
@@ -0,0 +1,9 @@
+
+// const myObj = {firstName:"Eke", lastName:"Oje"};
+// console.log(myObj);
+
+// const myObj = {firsname:"John", lastname:"Doe"};
+// console.log(myObj);
+
+const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
+console.log(myArr);
