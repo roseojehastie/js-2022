c7939845a2f0c91534f8893fdf24d4619f106186
Rose Hastie <roseojehastie@gmail.com>
2022-02-05 22:03:24 +0000

    add console

--- console.js
+++ console.js
@@ -5,5 +5,12 @@
 // const myObj = {firsname:"John", lastname:"Doe"};
 // console.log(myObj);
 
-const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
-console.log(myArr);
+// const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
+// console.log(myArr);
+
+// let x = 15 * 5;
+// // debugger;
+// document.getElementById(demo).innerHTML = x;
+let x = 15 * 5;
+debugger;
+document.getElementById("demo").innerHTML = x;
